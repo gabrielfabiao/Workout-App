169d536c9215ea624eb2eaaa40efb2f742f0856b
Gabriel Rodrigues <gabrielfilipedic@gmail.com>
2025-08-28 17:31:07 +0100

    solving calls

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -18,6 +18,5 @@ export const youtubeOptions = {
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options)
     const data = await response.json()
-
     return data
 }
